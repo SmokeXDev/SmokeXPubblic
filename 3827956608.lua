de2a1c1d6168388d0310832a0faff81913074f0f
SmokeX <111679379+SmokeXDev@users.noreply.github.com>
2022-12-08 17:51:57 +0100

    Create 3827956608.lua

---
+++ 3827956608.lua
@@ -0,0 +1,11 @@
+game.StarterGui:SetCore("SendNotification", {
+    Title = "Creator";
+    Text = "Made by SmokeXDev#8560"; 
+    Duration = 20;
+})
+wait(1)
+game.StarterGui:SetCore("SendNotification", {
+    Title = "SmokeX Client + Vape V4";
+    Text = "Pubblic Edition"; 
+    Duration = 20;
+})
