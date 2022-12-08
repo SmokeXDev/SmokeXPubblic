623837ced2cb811a31180a3a574f29db0c39a0aa
SmokeX <111679379+SmokeXDev@users.noreply.github.com>
2022-12-08 17:17:39 +0100

    Update NewMainForRoblox.lua

--- NewMainForRoblox.lua
+++ NewMainForRoblox.lua
@@ -1,3 +1,1 @@
-if game.PlaceId == 6872274481 then
- loadstring(game:HttpGet(('https://raw.githubusercontent.com/SmokeXDev/SmokeXPubblic/main/CustomModules/6872274481.lua'),true))()   
-end
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/SmokeXDev/SmokeXPubblic/main/CustomModules/6872274481.lua'),true))()
