c0f0af2e9babab3b74559b7dd7a147130bd0168b
SmokeX <111679379+SmokeXDev@users.noreply.github.com>
2022-12-08 17:29:01 +0100

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,12 +1,1 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/SmokeXDev/SmokeXPubblic/main/NewMainForRoblox.lua", true))()
-game.StarterGui:SetCore("SendNotification", {
-    Title = "Creator";
-    Text = "Made by SmokeXDev#8560"; 
-    Duration = 20;
-})
-wait(1)
-game.StarterGui:SetCore("SendNotification", {
-    Title = "SmokeX Client + VapeV4";
-    Text = "Pubblic Version"; 
-    Duration = 20;
-})
