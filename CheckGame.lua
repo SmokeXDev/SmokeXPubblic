048516200aa3ae1fbba52b26b62fbe71113419bc
SmokeX <111679379+SmokeXDev@users.noreply.github.com>
2022-12-09 12:51:03 +0100

    Update CheckGame.lua

--- CheckGame.lua
+++ CheckGame.lua
@@ -1,5 +1,5 @@
 if game.PlaceId == 6872274481 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/SmokeXDev/SmokeXPubblic/main/Loaders/6872274481.lua", true))()
+  print("Game")
 elseif game.PlaceId == 6872265039 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/SmokeXDev/SmokeXPubblic/main/Loaders/6872265039.lua", true))()
+  print("Lobby")
 end
