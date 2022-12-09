70110960a7cf04b6037255f1f977a7ea73e417a4
SmokeX <111679379+SmokeXDev@users.noreply.github.com>
2022-12-09 12:28:25 +0100

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,2 +1,1 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/SmokeXDev/SmokeXPubblic/main/NewMainForRoblox.lua", true))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/NewMainScript.lua", true))()
