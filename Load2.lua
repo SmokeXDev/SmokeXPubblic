00608825be6cc5e5c6250f91ac2dc74cc5de0afd
SmokeX <111679379+SmokeXDev@users.noreply.github.com>
2022-12-09 13:32:45 +0100

    Update and rename VapeV4.lua to Load2.lua

--- Load2.lua
+++ Load2.lua
@@ -1,1 +1,2 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/NewMainScript.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SmokeXDev/SmokeXPubblic/main/3827956608.lua", true))()
