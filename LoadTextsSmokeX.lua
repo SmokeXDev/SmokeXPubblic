bbd0a9e699ee1baeb2088e67f53e538ba2d377d6
SmokeX <111679379+SmokeXDev@users.noreply.github.com>
2022-12-09 13:48:44 +0100

    Create LoadTextsSmokeX.lua

---
+++ LoadTextsSmokeX.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SmokeXDev/SmokeXPubblic/main/3827956608.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SmokeXDev/SmokeXPubblic/main/6872274481.lua", true))()
