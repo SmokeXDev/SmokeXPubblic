a1890dd0f220725cdbf2c6898f9c2af50262c73b
SmokeX <111679379+SmokeXDev@users.noreply.github.com>
2022-12-09 12:17:17 +0100

    Update NewMainForRoblox.lua

--- NewMainForRoblox.lua
+++ NewMainForRoblox.lua
@@ -1,1 +1,2 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/SmokeXDev/SmokeXPubblic/main/Loaders/6872274481.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SmokeXDev/SmokeXPubblic/main/3827956608.lua", true))()
